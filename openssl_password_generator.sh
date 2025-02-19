f28009e7042db3d4f34a23d691f7c788fd2230b3
Nynros <37692542+Nynros@users.noreply.github.com>
2025-02-19 09:30:07 +0100

    Update openssl_password_generator.sh

--- openssl_password_generator.sh
+++ openssl_password_generator.sh
@@ -1,5 +1,2 @@
 #!/bin/bash
-for i in 1 2 3 4 5 6 7 8 9
-do
-  echo "$(openssl rand -base64 5)$(openssl rand -base64 5)$(openssl rand -base64 5)$(openssl rand -base64 5)"|openssl dgst -sha3-256
-done
+ echo "Neues Passwort: $(openssl rand -base64 20)"
